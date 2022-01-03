8636348ada6488ef8aaa10ff7d2593ef2aed4dca
BONG-KYO SEO <wenry55@gmail.com>
2022-01-03 10:00:48 +0900

    .

--- pages/Login.vue
+++ pages/Login.vue
@@ -28,6 +28,22 @@
                 </v-form>
               </v-card-text>
               <v-card-actions>
+                <v-btn
+                  color="primary"
+                  text
+                  @click="AxiosTest"
+                  :disabled="!valid"
+                >
+                  AxiosTest
+                </v-btn>
+                <v-btn
+                  color="primary"
+                  text
+                  @click="AxiosTestWithNuxt"
+                  :disabled="!valid"
+                >
+                  AxiosTestWithNuxt
+                </v-btn>
                 <v-spacer></v-spacer>
                 <v-btn
                   color="primary"
@@ -36,6 +52,14 @@
                   @click="userLogin"
                 >
                   LOGIN
+                </v-btn>
+                <v-btn
+                  color="primary"
+                  text
+                  :disabled="!valid"
+                  @click="userLoginGoogle"
+                >
+                  LOGIN WITH GOOGLE
                 </v-btn>
               </v-card-actions>
             </v-card>
@@ -47,6 +71,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'LoginForm',
   layout: 'LoginLayout',
@@ -61,11 +87,44 @@ export default {
     }
   },
 
+  beforeCreate() {
+    console.log('beforeCreate')
+    console.log(this.$route)
+
+    if (this.$route.query.code) {
+      console.log('got code')
+      // this.$axios.get("/auth/user").then(response => {
+      //   console.log(response)
+      // })
+      // this.$router.push("/userlist")
+      // this.$axios.setToken('123', 'Bearer')
+      // this.$axios.get('/auth/google/user?code=' + this.$route.query.code)
+      //   .then(response => {
+      //     console.log('response~~')
+      //   })
+      //   .catch(error => {
+      //     console.log(error)
+      //   })
+    }
+  },
+
+  // beforeMount() {
+  //   console.log('beforeMount')
+  //   console.log(this.$route)
+  // },
+
+  // mounted() {
+  //   console.log('mounted')
+  //   console.log(this.$route)
+  // },
+
   methods: {
     async userLogin() {
       // const response = await this.$auth.loginWith('local', { data: this.login })
       try {
-        const response = await this.$auth.loginWith('local', { data: this.login })
+        const response = await this.$auth.loginWith('local', {
+          data: this.login,
+        })
         console.log(response)
         this.$router.push({ name: 'Dashboard' })
       } catch (error) {
@@ -82,8 +141,26 @@ export default {
       // console.log(this.$auth.user)
       // console.log(this.$auth.loggedIn)
       // this.$router.push('dashboard')
+    },
+    async userLoginGoogle() {
+      try {
+        console.log('userLoginGoogle')
+        const response = await this.$auth.loginWith('google')
+        console.log(response)
+        this.$router.push({ name: 'Dashboard' })
+      } catch (error) {
+        this.$toast.error(error.message)
+      }
+    },
 
-
+    async AxiosTest() {
+      // const response = await this.$axios.$get('/auth/user') // !same as => axios.get('/api/auth/user')
+      const response = await axios.get('/api/auth/user') // !same as => axios.get('/api/auth/user')
+      console.log(response)
+    },
+    async AxiosTestWithNuxt() {
+      const response = await this.$axios.$get('/auth/user') // !same as => axios.get('/api/auth/user')
+      console.log(response)
     },
   },
 }
